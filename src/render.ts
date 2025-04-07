c3aecbaef051a0d70808862150c7fe3ef5b99b75
maikdevries <dev.maik.devries@outlook.com>
2025-04-07 23:21:46 +0200

    feat: escape unsafe HTML in expressions

--- src/render.ts
+++ src/render.ts
@@ -1,8 +1,22 @@
+const pattern = /[&<>"']/g;
+
+const escapes = {
+	'&': '&amp;',
+	'<': '&lt;',
+	'>': '&gt;',
+	'"': '&quot;',
+	"'": '&apos;',
+} as const;
+
+function escape(value: unknown): string {
+	return String(value).replace(pattern, (c) => escapes[c]);
+}
+
 function* render(strings: TemplateStringsArray, ...expressions: unknown[]): Generator<string> {
 	for (const [i, string] of strings.entries()) {
 		yield string;
 
 		const expr = expressions[i];
-		if (expr) yield String(expr);
+		if (expr) yield escape(expr);
 	}
 }
