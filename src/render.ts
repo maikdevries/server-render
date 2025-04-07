667c9152c8387dfa33e65dd071d0786f798a7cc2
maikdevries <dev.maik.devries@outlook.com>
2025-04-07 23:40:08 +0200

    feat: implement render to consume complete tagged template generator

--- src/render.ts
+++ src/render.ts
@@ -12,11 +12,19 @@ function escape(value: unknown): string {
 	return String(value).replace(pattern, (c) => escapes[c]);
 }
 
-function* render(strings: TemplateStringsArray, ...expressions: unknown[]): Generator<string> {
+function* html(strings: TemplateStringsArray, ...expressions: unknown[]): Generator<string> {
 	for (const [i, string] of strings.entries()) {
 		yield string;
 
 		const expr = expressions[i];
 		if (expr) yield escape(expr);
 	}
+}
+
+function render(template: Generator<string>): string {
+	let output = '';
+
+	for (const chunk of template) output += chunk;
+
+	return output;
 }
