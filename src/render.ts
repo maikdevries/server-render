8c3cb78dfbff581573f51316367229b496cf0d83
maikdevries <dev.maik.devries@outlook.com>
2025-04-08 13:19:48 +0200

    feat: support nested tagged templates

--- src/render.ts
+++ src/render.ts
@@ -12,11 +12,17 @@ function escape(value: unknown): string {
 	return String(value).replace(pattern, (c) => escapes[c]);
 }
 
+function isIterable(value: unknown): value is Iterable<string> | AsyncIterable<string> {
+	return typeof value !== 'string' && (value?.[Symbol.iterator] || value?.[Symbol.asyncIterator]);
+}
+
 async function* html([initial, ...strings]: TemplateStringsArray, ...expressions: unknown[]): AsyncGenerator<string> {
 	yield initial;
 
 	for (const [i, string] of strings.entries()) {
-		yield escape(await expressions[i]);
+		if (isIterable(expressions[i])) yield* expressions[i];
+		else yield escape(await expressions[i]);
+
 		yield string;
 	}
 }
