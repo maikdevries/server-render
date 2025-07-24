e7599a9f5fc983960cde39f7994b6dccc21800b9
maikdevries <dev.maik.devries@outlook.com>
2025-07-24 17:47:04 +0200

    refactor: expose module exports as re-exported namespace

--- mod.ts
+++ mod.ts
@@ -1,7 +1,1 @@
-import { html, stream, stringify } from './src/render.ts';
-
-export const render = {
-	'html': html,
-	'stream': stream,
-	'stringify': stringify,
-};
+export * as render from './src/render.ts';
